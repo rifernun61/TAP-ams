a45999e9e50f70027796090d9d8449ccaadeeeb0
Alunos --. Laboratório de T.I <aluno@lti.fatecso.edu.br>
2026-03-02 13:50:05 -0300

    feat: Project TAPAMS

---
+++ tapams/src/main/java/com/tapamsrichard/tapams/TapamsApplication.java
@@ -0,0 +1,13 @@
+package com.tapamsrichard.tapams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapamsApplication.class, args);
+	}
+
+}
---
+++ tapams/src/test/java/com/tapamsrichard/tapams/TapamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapamsrichard.tapams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
