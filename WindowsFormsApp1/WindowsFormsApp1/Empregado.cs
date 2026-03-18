071390b26514d136195fdeba4b8b014cda0681aa
Alunos --. Laboratório de T.I <aluno@lti.fatecso.edu.br>
2026-03-18 15:18:26 -0300

    feat: Add Windows Form App

---
+++ WindowsFormsApp1/WindowsFormsApp1/Empregado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAPAula
+{
+    abstract internal class Empregado
+    {
+        private int matricula;
+        private string nomeEmpregado;
+        private DateTime dataEntradaEmpresa;
+
+        public int Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public string NomeEmpregado
+        {
+            get { return nomeEmpregado; }
+            set { nomeEmpregado = value; }
+        }
+      
+        public DateTime DataEntradaEmpresa
+        {
+            get { return dataEntradaEmpresa; }
+            set { dataEntradaEmpresa = value; }
+        }
+
+        public virtual int TempoTrabalho()
+        {
+            TimeSpan span =
+                DateTime.Today.Subtract(DataEntradaEmpresa);
+            return span.Days;
+        }
+
+        public abstract double SalarioBruto();
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Mensalista.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAPAula
+{
+    internal class Mensalista : Empregado
+    {
+        public Double SalarioMensal { get; set; }
+
+        public override double SalarioBruto()
+        {
+            return this.SalarioMensal;
+        }
+    }
+}
