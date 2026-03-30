2cde35a8a863187072b88ae4e8f4ee91e7fe83fe
Alunos --. Laboratório de T.I <aluno@lti.fatecso.edu.br>
2026-03-30 14:43:48 -0300

    feat(music): Add /{id} route

--- music/src/main/java/com/richard/music/music/controller/MusicController.java
+++ music/src/main/java/com/richard/music/music/controller/MusicController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,15 @@ public class MusicController {
     public List<Music> getAllMusics() {
         // returning hardcoded registers for now
         List<Music> musics = new ArrayList<>();
-        musics.add(new Music(1L, "Bohemian Rhapsody", "Queen"));
-        musics.add(new Music(2L, "Imagine", "John Lennon"));
-        musics.add(new Music(3L, "Hotel California", "Eagles"));
+        musics.add(new Music(1L, "Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975));
+        musics.add(new Music(2L, "Imagine", "John Lennon", "Imagine", 1971));
+        musics.add(new Music(3L, "Hotel California", "Eagles", "Hotel California", 1976));
         return musics;
+    }
+
+    @GetMapping("/{id}")
+    public Music getMusic(@PathVariable Long id) {
+        // returning hardcoded register for now
+        return new Music(id, "Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975);
     }
 }
--- music/src/main/java/com/richard/music/music/model/Music.java
+++ music/src/main/java/com/richard/music/music/model/Music.java
@@ -4,19 +4,25 @@ public class Music {
     private Long id;
     private String title;
     private String artist;
+    private String album;
+    private Integer releaseYear;
 
     public Music() {
     }
 
-    public Music(String title, String artist) {
+    public Music(String title, String artist, String album, Integer releaseYear) {
         this.title = title;
         this.artist = artist;
+        this.album = album;
+        this.releaseYear = releaseYear;
     }
 
-    public Music(Long id, String title, String artist) {
+    public Music(Long id, String title, String artist, String album, Integer releaseYear) {
         this.id = id;
         this.title = title;
         this.artist = artist;
+        this.album = album;
+        this.releaseYear = releaseYear;
     }
 
     public Long getId() {
@@ -43,4 +49,19 @@ public class Music {
         this.artist = artist;
     }
 
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
 }
